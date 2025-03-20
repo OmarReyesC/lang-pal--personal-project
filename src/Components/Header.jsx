5bcf4f796b034e67d4be7ad9928a676b4f0007ed
Omar Reyes <omareyescar@gmail.com>
2025-03-20 11:14:18 -0600

    Fixed navbar styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,24 @@ import ProfileIconUrl from '../assets/profile-icon.svg';
 export default function Header() {
     return (
         <header className="header">
-            <Link className="header__logo headline">Lang-Pal</Link>
+            <Link to="/" className="header__logo headline">Lang-Pal</Link>
             <nav className="header__navbar">
-                <NavLink className="label" >My learning</NavLink>
-                <NavLink className="label" >Live Classes</NavLink>
+                <NavLink 
+                    to="my-learning"
+                    className={({ isActive }) =>
+                        isActive ? 'active label' : 'label'
+                    } 
+                >
+                    My learning
+                </NavLink>
+                <NavLink 
+                    to="live-classes"
+                    className={({ isActive }) =>
+                        isActive ? 'active label' : 'label'
+                    } 
+                >
+                    Live Classes
+                </NavLink>
                 <Link className="profile-link" >
                     <img src={ProfileIconUrl} alt="Profile" />
                 </Link>
